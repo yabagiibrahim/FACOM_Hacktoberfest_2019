1715b47003dddf8441e74b4e63e53aab45adb197
yabagiibrahim <55027665+yabagiibrahim@users.noreply.github.com>
2019-10-07 03:40:24 +0100

    Update area.py
    I think both actually works

--- Python/circle_area.py
+++ Python/circle_area.py
@@ -3,3 +3,13 @@ print("Type the circle radius :")
 x = float(input())
 
 print("Area : ",round(3.1415926*(x*x),4))
+
+
+This can also be done easily using a function(areacalc)
+>>>def area_calc(r):
+     import math
+     return math.pi* pow(r,2)
+
+radius=int(input('enter radius:'))
+area_calc(2)
+print area_calc(2)
